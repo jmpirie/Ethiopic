616642f57c15f76edf4e5d473e6b3640b74cf5f5
John Michael Pirie <jmpirie@gmail.com>
2017-04-14 04:03:55 -0600

    fireabse in dagger; initial injects

---
+++ app/src/main/java/com/hundaol/ethiocal/AnalyticsManager.java
@@ -0,0 +1,8 @@
+package com.hundaol.ethiocal;
+
+/**
+ * Created by john.pirie on 2017-04-14.
+ */
+
+public class AnalyticsManager {
+}
--- app/src/main/java/com/hundaol/ethiocal/App.java
+++ app/src/main/java/com/hundaol/ethiocal/App.java
@@ -4,18 +4,31 @@ import android.app.Application;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
+import javax.inject.Inject;
+
 /**
  * Created by john.pirie on 2017-04-14.
  */
 
 public class App extends Application {
 
-    private FirebaseAnalytics firebaseAnalytics;
+    @Inject
+    FirebaseAnalytics firebaseAnalytics;
+
+    private static AppComponent appComponent;
+
+    public static AppComponent getAppComponent() {
+        return appComponent;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        appComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
+
+        getAppComponent().inject(this);
     }
 }
