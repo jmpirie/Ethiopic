f3812d1625ffea9f4044633514c73a2dcd53964a
John Michael Pirie <jmpirie@gmail.com>
2017-04-17 01:27:56 -0600

    wiring in butterknife

--- app/src/main/java/com/hundaol/ethiopic/MainActivity.java
+++ app/src/main/java/com/hundaol/ethiopic/MainActivity.java
@@ -5,13 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.hundaol.ethiocal.R;
+import com.hundaol.ethiopic.views.CalendarView;
 
 import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     @Inject
     FirebaseAnalytics firebaseAnalytics;
+
+    @BindView(R.id.cal)
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,7 @@ public class MainActivity extends AppCompatActivity {
         App.getAppComponent().inject(this);
 
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
