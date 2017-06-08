ae52437990d87e18af4fc330c4cc76ed143a10af
John Michael Pirie <jmpirie@gmail.com>
2017-06-08 07:54:49 -0600

    debouncing calendar events

--- app/src/main/java/com/hundaol/ethiopic/views/DeviceCalendarEventViewModel.kt
+++ app/src/main/java/com/hundaol/ethiopic/views/DeviceCalendarEventViewModel.kt
@@ -3,12 +3,12 @@ package com.hundaol.ethiopic.viewmodels
 import android.content.Context
 import android.graphics.drawable.Drawable
 
-import com.hundaol.ethiopic.domain.DeviceCalendar
+import com.hundaol.ethiopic.domain.CalendarEvent
 
 /**
  * Created by abinet on 6/6/17.
  */
 
-class DeviceCalendarEventViewModel(val context: Context, var deviceCalendar: DeviceCalendar) {
+class DeviceCalendarEventViewModel(val context: Context, var calendarEvent: CalendarEvent) {
     var backgroundIndicator: Drawable? = null
 }
